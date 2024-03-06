5bc8c4a8de0920e6fd0dae5fa2ce25676dce02a8
blacktache <adnanemandili@gmail.com>
2024-03-06 16:48:53 +0100

    update adding an other type

--- schema/schema.js
+++ schema/schema.js
@@ -1,107 +1,142 @@
+// schema.js
+
+// Import necessary GraphQL modules
 const { 
-    GraphQLObjectType, // Defines a new GraphQL object type
-    GraphQLString,     // Represents a string type in GraphQL
-    GraphQLSchema,     // Creates a GraphQL schema that defines the entire GraphQL API
-    GraphQLList,       // Represents a list type in GraphQL
+    GraphQLObjectType,     // Define GraphQL object types
+    GraphQLString,         // Define GraphQL string type
+    GraphQLSchema,         // Define GraphQL schema
+    GraphQLList,           // Define GraphQL list type
+    GraphQLNonNull         // Define GraphQL non-null type
 } = require('graphql');
 
-const { Pool } = require('pg'); // Imports the PostgreSQL client library
+// Import the PostgreSQL client
+const { Pool } = require('pg');
 
-// Creates a PostgreSQL connection pool
+// Create a PostgreSQL connection pool
 const pool = new Pool({
-    user: 'postgres',    // PostgreSQL username
-    host: 'localhost',   // PostgreSQL host
-    database: 'Books',   // PostgreSQL database name
-    password: 'admin',   // PostgreSQL password
-    port: 5432           // PostgreSQL port
+    user: 'postgres',          // PostgreSQL username
+    host: 'localhost',         // PostgreSQL host
+    database: 'Books',         // PostgreSQL database name
+    password: 'admin',         // PostgreSQL password
+    port: 5432                 // Default PostgreSQL port
 });
 
-// Defines the GraphQL Book type
-const BookType = new GraphQLObjectType({
-    name: "Book",             // Name of the GraphQL object type
-    fields: () => ({          // Defines the fields of the Book type
-        id: { type: GraphQLString },    // Represents the ID of the book
-        name: { type: GraphQLString },  // Represents the name of the book
-        genre: { type: GraphQLString }  // Represents the genre of the book
+// Define the Author type
+const AuthorType = new GraphQLObjectType({
+    name: "Author",                 // Name of the GraphQL object type
+    fields: () => ({                // Define the fields of the Author type
+        id: { type: GraphQLString },   // Author ID
+        name: { type: GraphQLString }, // Author name
+        books: {                       // List of books written by the author
+            type: new GraphQLList(BookType),
+            resolve(parent, args) {
+                // Retrieve books written by this author from the database
+                return pool.query('SELECT * FROM book WHERE author_id = $1', [parent.id])
+                    .then(res => res.rows);
+            }
+        }
     })
 });
 
-// Defines the root query for retrieving data (GET mathod)
+// Define the Book type
+const BookType = new GraphQLObjectType({
+    name: "Book",                   // Name of the GraphQL object type
+    fields: () => ({                // Define the fields of the Book type
+        id: { type: GraphQLString },      // Book ID
+        name: { type: GraphQLString },    // Book name
+        genre: { type: GraphQLString },   // Book genre
+        authorId: { type: GraphQLString } // Author ID associated with the book
+    })
+});
+
+// Define the Root Query
 const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",    // Name of the root query type
+    name: "RootQueryType",         // Name of the root query type
     fields: {
-        book: {               // Defines a query to get a single book by ID
-            type: BookType,   // Specifies the return type of the query
-            args: {           // Specifies the arguments for the query
+        // Query to get a single book by ID
+        book: {
+            type: BookType,        // Return type of the query
+            args: {
                 id: { type: GraphQLString } // ID of the book to retrieve
             },
-            async resolve(parent, args) {   // Resolves the query
-                // Retrieves book data from the PostgreSQL database
-                const { rows } = await pool.query('SELECT * FROM book WHERE id = $1', [args.id]);
-                return rows[0];  // Returns the book data
+            resolve(parent, args) {
+                // Resolve function to fetch book data from the database
+                return pool.query('SELECT * FROM book WHERE id = $1', [args.id])
+                    .then(res => res.rows[0]);
             }
         },
-        books: {              // Defines a query to get all books
-            type: new GraphQLList(BookType),  // Specifies the return type of the query
-            async resolve(parent, args) {     // Resolves the query
-                // Retrieves all book data from the PostgreSQL database
-                const { rows } = await pool.query('SELECT * FROM book');
-                return rows;   // Returns all book data
+        // Query to get all books
+        books: {
+            type: new GraphQLList(BookType), // Return type of the query
+            resolve(parent, args) {
+                // Resolve function to fetch all books from the database
+                return pool.query('SELECT * FROM book')
+                    .then(res => res.rows);
+            }
+        },
+        // Query to get a single author by ID
+        author: {
+            type: AuthorType,       // Return type of the query
+            args: {
+                id: { type: GraphQLString } // ID of the author to retrieve
+            },
+            resolve(parent, args) {
+                // Resolve function to fetch author data from the database
+                return pool.query('SELECT * FROM author WHERE id = $1', [args.id])
+                    .then(res => res.rows[0]);
+            }
+        },
+        // Query to get all authors
+        authors: {
+            type: new GraphQLList(AuthorType), // Return type of the query
+            resolve(parent, args) {
+                // Resolve function to fetch all authors from the database
+                return pool.query('SELECT * FROM author')
+                    .then(res => res.rows);
             }
         }
     }
 });
 
-// Defines mutations for modifying data (POST,PUT,DELETE methods)
+// Define Mutations
 const Mutation = new GraphQLObjectType({
-    name: 'Mutation',    // Name of the mutation type
+    name: 'Mutation',          // Name of the mutation type
     fields: {
-        addBook: {        // Mutation to add a new book
-            type: BookType,  // Specifies the return type of the mutation
-            args: {          // Specifies the arguments for the mutation
-                name: { type: GraphQLString },   // Name of the new book
-                genre: { type: GraphQLString }   // Genre of the new book
+        // Mutation to add a new book
+        addBook: {
+            type: BookType,     // Return type of the mutation
+            args: {
+                name: { type: GraphQLString },       // Name of the new book
+                genre: { type: GraphQLString },      // Genre of the new book
+                authorId: { type: GraphQLString }    // ID of the author who wrote the book
             },
-            async resolve(parent, args) {     // Resolves the mutation
-                const { name, genre } = args;  // Destructures arguments
-                // Inserts a new book into the PostgreSQL database
-                const { rows } = await pool.query('INSERT INTO book(name, genre) VALUES($1, $2) RETURNING *', [name, genre]);
-                return rows[0];   // Returns the newly added book
+            resolve(parent, args) {
+                // Resolve function to add a new book to the database
+                const { name, genre, authorId } = args;
+                return pool.query('INSERT INTO book(name, genre, author_id) VALUES($1, $2, $3) RETURNING *', [name, genre, authorId])
+                    .then(res => res.rows[0]);
             }
         },
-        updateBook: {     // Mutation to update an existing book
-            type: BookType,  // Specifies the return type of the mutation
-            args: {           // Specifies the arguments for the mutation
-                id: { type: GraphQLString },   // ID of the book to update
-                name: { type: GraphQLString }, // New name for the book
-                genre: { type: GraphQLString } // New genre for the book
+        // Mutation to add a new author
+        addAuthor: {
+            type: AuthorType,    // Return type of the mutation
+            args: {
+                name: { type: GraphQLString } // Name of the new author
             },
-            async resolve(parent, args) {   // Resolves the mutation
-                const { id, name, genre } = args;  // Destructures arguments
-                // Updates an existing book in the PostgreSQL database
-                const { rows } = await pool.query('UPDATE book SET name = $1, genre = $2 WHERE id = $3 RETURNING *', [name, genre, id]);
-                return rows[0];   // Returns the updated book
-            }
-        },
-        deleteBook: {     // Mutation to delete an existing book
-            type: BookType,  // Specifies the return type of the mutation
-            args: {           // Specifies the arguments for the mutation
-                id: { type: GraphQLString } // ID of the book to delete
-            },
-            async resolve(parent, args) {   // Resolves the mutation
-                const { id } = args;   // Destructures arguments
-                // Deletes an existing book from the PostgreSQL database
-                const { rows } = await pool.query('DELETE FROM book WHERE id = $1 RETURNING *', [id]);
-                return rows[0];   // Returns the deleted book
+            resolve(parent, args) {
+                // Resolve function to add a new author to the database
+                const { name } = args;
+                return pool.query('INSERT INTO author(name) VALUES($1) RETURNING *', [name])
+                    .then(res => res.rows[0]);
             }
         }
     }
 });
 
-// Creates the GraphQL schema that combines the root query and mutations
+// Create the GraphQL Schema
 const schema = new GraphQLSchema({
-    query: RootQuery,    // Specifies the root query
-    mutation: Mutation   // Specifies the mutations
+    query: RootQuery,           // Root query for retrieving data
+    mutation: Mutation          // Mutation for modifying data
 });
 
-module.exports = schema;   // Exports the GraphQL schema for use in the application
+module.exports = schema;        // Export the GraphQL schema for use in the application
